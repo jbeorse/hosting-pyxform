caabbf0e2019f23375e347e96ddddb37e9abe137
Jeff Beorse <Jeff@Beorse.net>
2011-07-23 21:54:30 -0700

    Url pathing to only use id and error arguments. ID indicates the specific file to be downloaded. This should fix the download security issue.

--- convert.py
+++ convert.py
@@ -1,14 +1,13 @@
 import os, sys, random
+from index import download_dir
 from pyxform.pyxform.builder import create_survey_from_path
-
-# Download directory
-download_dir = 'xforms/'
 
 def convert(upload_path):
     path_to_excel_file = upload_path
     survey = create_survey_from_path(path_to_excel_file)
-    download_path = download_dir + str(int(random.random()*1000000000000))
+    ID = str(int(random.random()*1000000000000))
+    download_path = download_dir + ID
     os.mkdir(download_path)
     path_to_xform = os.path.join(download_path, survey.get_name() + ".xml")
     survey.print_xform_to_file(path_to_xform, False)
-    return path_to_xform
+    return ID
--- index.py
+++ index.py
@@ -1,41 +1,121 @@
-import web, os
-import upload
-import download
+import web, os, cgi, time, sys, random, shutil
+from stat import *
+import convert
 
+web.webapi.internalerror = web.debugerror
+web.config.debug=True
 
 # Download expire time
 exp = 3600 # 1 hour
 
-rootdir = os.path.abspath(os.path.dirname('templates/')) + '/'
-render = web.template.render(rootdir)
+# Maximum file input size
+cgi.maxlen = 50*1024*1024 # 50MB
+
+# Upload expire time
+exp = 600 # 10 minutes
+
+# Directory to upload xls files to
+upload_dir = 'xls_files'
+
+# Download directory
+download_dir = 'xforms/'
+
+# Director for templates
+rootdir = os.path.abspath('templates') + '/'
+template = web.template.render(rootdir)
         
 urls = (
-    '/upload', 'upload.Upload',
-    '/index', 'Index',
-    '/index/(.*)', 'Index',
-    '/xform/(.*)', 'download.Download'
+    '/', 'Index',
+    '/(.*)', 'Index'
 )
-app = web.application(urls, globals())
+app = web.application(urls, globals(), autoreload=False)
+main = app.wsgifunc()
 
 class Index:        
     def GET(self, name=None):
-        return render.index(name)
+        args = web.input()
+        #If an error was raised, display it
+        if 'e' in args:
+            return template.index(error=args.e)
+            
+        #If an id is provided, then the conversion is complete
+        elif 'id' in args:
+            #If d is defined the user clicked the download link
+            if 'd' in args:
+                self.cleanup_download_files()
+                dname = download_dir+args.id
+                print "dname = " + dname
+                files = os.listdir(dname)
+                print "file = " + files[0]
+                web.header('Content-Disposition', 'attachment; filename='+files[0])
+                f = open(dname+'/'+files[0],'r')
+                return f.read()
+                
+            #Otherwise just offer the link to download
+            else:
+                return template.index(id=args.id)
         
-    def cleanup_files(self):
-        files = os.listdir(download_dir)
-        for f in files:
-            web.debug("Investigating: " + download_dir + f)
-            st=os.stat(download_dir + f)
+        #Else display the default page
+        else:
+            return template.index()
+    
+    #Uploading an xls file
+    def POST(self):
+        try:
+            args = web.input(xlsfile={}) 
+        except ValueError:
+            return "File too large. Maximum file size is 50MB"
+            
+        if 'xlsfile' not in args:
+            web.seeother('/')
+        
+        #Cleanup the file path  
+        filepath = args.xlsfile.filename.replace('\\','/')
+        filename = filepath.split('/')[-1]
+        
+        if not filename.lower().endswith('xls'):
+            raise web.seeother('/?e=Only xls files are accepted')
+        
+        #Generate a unique folder to store the uploaded file in    
+        ID = str(int(random.random()*1000000000000))
+        os.mkdir(upload_dir+'/'+ID)  
+        
+        #Store the uploaded xls 
+        fout = open(upload_dir +'/'+ID+'/'+filename,'w') 
+        fout.write(args.xlsfile.file.read())
+        fout.close()
+        
+        #Remove any expired uploads
+        self.cleanup_upload_files()
+        
+        #Convert the file. Report any errors, or offer the id of the
+        #folder to download from
+        try:
+            download_ID = convert.convert(upload_dir +'/'+ID+'/'+filename)
+        except:
+            raise web.seeother('?e='+ str(sys.exc_info()[1]))
+        raise web.seeother('/?id=' + download_ID)
+    
+    #Iterate through the upload folder and remove any expired folders    
+    def cleanup_upload_files(self):
+        dirs = os.listdir(upload_dir)
+        for d in dirs:
+            st=os.stat(upload_dir + '/' + d)
             mtime = st[ST_MTIME]
             
             if time.time() - mtime > exp:
-                for root, dirs, nested_files in os.walk(download_dir+f, topdown=False):
-                    for name in nested_files:
-                        os.remove(os.path.join(root, name))
-                    for name in dirs:
-                        os.rmdir(os.path.join(root, name))
-                os.rmdir(download_dir+f)
-                web.debug('Deleteing: ' + download_dir + f)
+                shutil.rmtree(upload_dir + '/' + d)
+                web.debug('Deleteing: ' + d)
+    
+    def cleanup_download_files(self):
+        dirs = os.listdir(download_dir)
+        for d in dirs:
+            st=os.stat(download_dir + d)
+            mtime = st[ST_MTIME]
+            
+            if time.time() - mtime > exp:
+                shutil.rmtree(download_dir+d)
+                web.debug('Deleteing: ' + download_dir + d)
 
 if __name__ == "__main__":
     app.run()
